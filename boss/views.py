052f1297be02202dedb3ca8f59e5ce2830b32d16
sukkwono-hits <sukkwono@hits.ai>
2021-12-03 23:42:18 +0900

    some changes in boss

--- boss/views.py
+++ boss/views.py
@@ -8,14 +8,21 @@ from rest_framework.parsers import JSONParser
 
 
 @csrf_exempt
-def time_input(request):
+def order_list(request, shop):
     if request.method=="GET":
-        order_list = Order.objects.all()
+        order_list = Order.objects.filter(shop=shop)
         return render(request, "boss/order_list.html", {"order_list": order_list})
+    else:
+        return HttpResponse(status=404)
 
-    elif request.method=="POST":
+@csrf_exempt
+def time_input(request):
+    if request.method=="POST":
         order_item = Order.objects.get(pk=request.POST["order_id"])
+        shop = order_item.shop.id
         order_item.estimated_time = request.POST["estimated_time"]
         order_item.save()
 
-        return render(request, "boss/success.html")
+        return render(request, "boss/success.html", {"shop": shop})
+    else:
+        return HttpResponse(status=404)
