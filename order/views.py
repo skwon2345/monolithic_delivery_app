b88d681add327fcbb2a2b73d02a3aeaadb64cdff
sukkwono-hits <sukkwono@hits.ai>
2021-12-03 11:42:43 +0900

    add Menu

--- order/views.py
+++ order/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
 from .models import Menu, Order, Orderfood, Shop
-from .serializers import ShopSerializer
+from .serializers import MenuSerializer, ShopSerializer
 
 
 @csrf_exempt
@@ -17,6 +17,21 @@ def shop(request):
     elif request.method == "POST":
         data = JSONParser().parse(request)
         serializer = ShopSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def menu(request):
+    if request.method == "GET":
+        menu = Menu.objects.all()
+        serializer = MenuSerializer(menu, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = MenuSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
