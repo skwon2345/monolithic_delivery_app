cc5f12f41ea693b4e92ba76e4e226f2f7f0daf2a
sukkwono-hits <sukkwono@hits.ai>
2021-12-03 17:19:46 +0900

    order: views: add order()

--- order/views.py
+++ order/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 
@@ -30,7 +31,7 @@ def menu(request, shop):
         menu = Menu.objects.filter(shop=shop)
         # serializer = MenuSerializer(menu, many=True)
         # return JsonResponse(serializer.data, safe=False)
-        return render(request, "order/menu_list.html", {"menu_list": menu})
+        return render(request, "order/menu_list.html", {"menu_list": menu, "shop": shop})
 
     elif request.method == "POST":
         data = JSONParser().parse(request)
@@ -39,3 +40,26 @@ def menu(request, shop):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def order(request):
+    if request.method == "POST":
+        address = request.POST["address"]
+        shop = request.POST["shop"]
+        food_list = request.POST.getlist("menu")
+        order_date = timezone.now()
+
+        shop_item = Shop.objects.get(pk=int(shop))
+
+        shop_item.order_set.create(address=address, order_date=order_date)
+
+        order_item = Order.objects.get(pk=shop_item.order_set.latest("id").id) # Order 테이블에 shop_item의 id를 외래키로 갖는 아이템중 가장 최근 것
+        for food in food_list:
+            order_item.orderfood_set.create(food_name=food)
+
+        return HttpResponse(status=200)
+
+    elif request.method == "GET":
+        order_list = Order.objects.all()
+        return render(request, "order/order_list.html", {"order_list": order_list})
+
