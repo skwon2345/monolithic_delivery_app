1f5a16fc21de5a45dd40ab572509b8c67974f062
sukkwono-hits <sukkwono@hits.ai>
2021-12-04 22:19:50 +0900

    settings: set Mysql RDS database

--- fastcampus/settings.py
+++ fastcampus/settings.py
@@ -77,13 +77,26 @@ WSGI_APPLICATION = "fastcampus.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "project1",
+        "USER": "admin",
+        "PASSWORD": "aws12345678",
+        "HOST": "database-1.cgpibwcpuhuw.us-west-2.rds.amazonaws.com",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
